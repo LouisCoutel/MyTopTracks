7443c1eae6c39d1c2ba7fd73a2d373ec22aad4a1
BastienVintras <bastvint0gmail.com>
2023-07-19 11:47:23 +0200

    commentaire JS

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+//Importer le module axios servant a gerer les requetes 
 const axios = require("axios");
 
+//Structure de la requete
 const options = {
   method: "GET",
+  //URL du Point d 'acces "End point"
   url: "https://shazam.p.rapidapi.com/charts/list",
   headers: {
     "X-RapidAPI-Key": "bb7644f4camsh704f7d5613a7768p12b59bjsn15758ea5893b",
     "X-RapidAPI-Host": "shazam.p.rapidapi.com",
   },
 };
-
+//Creation de la fonction asynchrone pour que JS attende la réponse de la requete avant de continuer
 async function test() {
   try {
     const response = await axios.request(options);
+    //le .data transforme la reponse en objet Json
     console.log(response.data);
   } catch (error) {
     console.error(error);
@@ -19,3 +23,4 @@ async function test() {
 }
 
 test();
+
