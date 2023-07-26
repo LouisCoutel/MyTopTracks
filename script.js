3875508b81b0bbe6d6e20fe48e542c6b95a41ed0
LouisCoutel <louiscoutel75@gmail.com>
2023-07-26 14:51:24 +0200

    tableau complet + elementHTML

--- script.js
+++ script.js
@@ -1,39 +1,25 @@
 
-// // fonction d'animation lors du survol des éléments
-// const hoverAnim = function (country, color1, color2) {
-//   // on enlève les précédentes targets 
-//   // pour éviter un comportement bizarre de répétition de l'animation
-//   anime.remove(country);
-//   // animejs + paramètres
-//   anime({
-//     // éléments cibles
-//     targets: `#${country.id}`,
-//     // équivalent de "background-color" pour les SVG
-//     fill: [color1, color2],
-//     // équivalent de "border"
-//     stroke: [color1, color2],
-//     // durée de l'animation
-//     duration: 1000,
-//   })
-// }
+// fonction d'animation lors du survol des éléments
+const hoverAnim = function (country, color1, color2) {
+  // on enlève les précédentes targets 
+  // pour éviter un comportement bizarre de répétition de l'animation
+  anime.remove(country);
+  // animejs + paramètres
+  anime({
+    // éléments cibles
+    targets: `#${country.id}`,
+    // équivalent de "background-color" pour les SVG
+    fill: [color1, color2],
+    // équivalent de "border"
+    stroke: [color1, color2],
+    // durée de l'animation
+    duration: 1000,
+  })
+}
+// fonction pour tous les pays
 
-// // fonction pour tous les pays
-// countries.forEach(country => {
-//   // on crée la propriété element qui fait le lien entre JS et le Document Object Model HTML
-//   country.element = document.getElementById(country.id);
-//   // lorsqu'on rentre dans la zone du pays avec le curseur
-//   country.element.onmouseenter = function () {
-//     // animation de blanc cassé à rouge
-//     hoverAnim(country, '#fefefa', '#ffe500').play;
-//   };
-//   // et l'inverse quand on quitte le pays pour revenir à l'état de base
-//   country.element.onmouseleave = function () {
-//     console.log('exit')
-//     hoverAnim(country, '#ffe500', '#fefefa').play;
-//   }
-// })
 
-// déclaration de fonction pour chopper la liste avec les listId
+//déclaration de fonction pour chopper la liste avec les listId
 async function getChartList() {
   const url = 'https://shazam.p.rapidapi.com/charts/list';
   const options = {
@@ -72,6 +58,22 @@ async function getCountryChart(listid, chartRange) {
   }
 }
 
+function setCountryElement(countries) {
+  countries.forEach(country => {
+    // on crée la propriété HTMLelement qui fait le lien entre JS et le Document Object Model HTML
+    country.HTMLelement = document.getElementById(country.id);
+    // lorsqu'on rentre dans la zone du pays avec le curseur
+    country.HTMLelement.onmouseenter = function () {
+      // animation de blanc cassé à jaune
+      hoverAnim(country, '#fefefa', '#ffe500').play;
+    };
+    // et l'inverse quand on quitte le pays pour revenir à l'état de base
+    country.HTMLelement.onmouseleave = function () {
+      console.log('exit')
+      hoverAnim(country, '#ffe500', '#fefefa').play;
+    }
+  })
+}
 
 // fonction qui sert juste à englober la séquence d'éxécution 
 // chartList -> countryCharts -> ?
@@ -79,22 +81,26 @@ async function mainFunction() {
   let countries = await getChartList();
 
   // test avec un seul pays
-  let tracks = await getCountryChart(countries[0].listid, 1);
-  console.log(tracks.title)
-    countries[0].title = tracks[0].title
-    countries[0].artist = tracks[0].subtitle
-    countries[0].images = tracks[0].images
+  //   setTimeout(async function () {
+  //    let tracks = await getCountryChart(countries[0].listid, 1);
+  //    countries[0].title = tracks[0].title
+  //    countries[0].artist = tracks[0].subtitle
+  //    countries[0].images = tracks[0].images
+  //    console.log(countries[0]);
+  //  }, 200)
 
-  console.log(countries[0]);
   // fin du test
 
-  // countries.forEach(country => {
-  //   let tracks = await getCountryChart(country.listid, 1);
-  //   country.title = tracks.title
-  //   country.artist = tracks.subtitle
-  //   country.images = tracks.images
-  // })
-  // console.log(await getCountryChart(countries[0].listid, 1))
+   for await (let country of countries) {
+     let tracks = await getCountryChart(country.listid, 1);
+     country.title = tracks[0].title
+     country.artist = tracks[0].subtitle
+     country.images = tracks[0].images
+     console.log("pays : " + country.id + ", chanson : " + country.title + ", artiste : " + country.artist + ", liste d'images : " + country.images)
+   }
+
+  setCountryElement(countries);
+  console.log(countries[0])
 }
 
 mainFunction();
